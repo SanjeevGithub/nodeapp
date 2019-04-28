8a57ff60206ae6253ac2db6962ecffcdb3305232
Sanjeev Desai <sdesai@and.digital>
2019-04-28 20:38:54 +0100

    updated

---
+++ app_alt.js
@@ -0,0 +1,21 @@
+
+
+const cpath = require('path');
+var vpath=cpath.parse(__filename);
+console.log(vpath);
+
+
+// function sayHello(name){
+//     console.log('Hi there...'+name)
+// }
+
+
+// sayHello('Pandurang')
+
+//var loggerApp= require('./logger.js');
+//console.log(loggerApp)
+//console.log(loggerApp.sdval)
+//console.log(loggerApp)
+
+//loggerApp.mylog('theek aahe. Coming from the function in logger module ')
+//console.log(module)
---
+++ logger.js
@@ -0,0 +1,9 @@
+
+function log(message){
+    console.log(message)
+}
+
+var val='Sanju baba'
+module.exports.mylog=log
+
+module.exports.sdval=val;
