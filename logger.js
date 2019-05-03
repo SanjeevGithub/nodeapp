7f87ec2a5422c024efb8d528c3bfd92f5c0efc2a
Sanjeev Desai <sdesai@and.digital>
2019-05-03 16:41:07 +0100

    events added

--- logger.js
+++ logger.js
@@ -1,9 +1,20 @@
 
-function log(message){
-    console.log(message)
+const EventListner = require('events')
+
+class Logger extends EventListner {
+
+    log(message){
+        console.log(message);
+        this.emit('Emittingthismessage',{id:1, fyd:2});
+    }
+
+
 }
 
-var val='Sanju baba'
-module.exports.mylog=log
 
-module.exports.sdval=val;
+module.exports=Logger;
+
+// var val='Sanju baba'
+// module.exports.mylog=log
+
+// module.exports.sdval=val;
